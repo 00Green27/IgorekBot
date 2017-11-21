c07e560a59a5f7bed59067da6f4b2548a623fc33
Taras Gorgul <taras.gorgul@gmail.com>
2017-11-21 11:49:26 +0300

    BLL added

---
+++ IgorekBot.BLL/Services/TimeSheetService.cs
@@ -0,0 +1,13 @@
+﻿using IgorekBot.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgorekBot.BLL.Services
+{
+    public class TimeSheetService : ITimeSheetService
+    {
+    }
+}
