4c987caa371140acd788db4cf08c3e12f35f1915
00Geen27 <patrick17@mail.ru>
2017-12-08 17:53:51 +0300

    service

--- src/IgorekBot.BLL/Services/ITimeSheetService.cs
+++ src/IgorekBot.BLL/Services/ITimeSheetService.cs
@@ -4,16 +4,16 @@ namespace IgorekBot.BLL.Services
 {
     public interface ITimeSheetService
     {
-        GetUserByIdResponse GetUserById(GetUserByIdRequest request);
-
         AddUserByEMailResponse AddUserByEmail(AddUserByEmailRequest request);
-
+        AddTimeSheetResponse AddTimeSheet(AddTimeSheetRequest request);
+        GetUserByIdResponse GetUserById(GetUserByIdRequest request);
+        //GetEmployeeByPhoneNo
+        GetUserProjectsResponse GetUserProjects(GetUserProjectsRequest request);
+        GetProjectTasksResponse GetProjectTasks(GetProjectTasksRequest request);
+        //TimeSheetsPerDay
+        //TimeSheetsPerWeek
         ValidatePasswordResponse ValidatePassword(ValidatePasswordRequest request);
 
-        GetUserProjectsResponse GetUserProjects(GetUserProjectsRequest request);
 
-        GetProjectTasksResponse GetProjectTasks(GetProjectTasksRequest request);
-
-        AddTimeSheetResponse AddTimeSheet(AddTimeSheetRequest request);
     }
 }
